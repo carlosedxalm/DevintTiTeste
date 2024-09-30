e9d269d67742e3c197c22e4a8ff5e7bbf8cbf28b
Carlos <carlosedx.alm@gmail.com>
2024-09-30 10:06:12 -0300

    Algumas alterações para resolução de bugs e adição de arquivo de estilização da pagina

--- obj/Debug/net8.0/DavinTI.AssemblyInfo.cs
+++ obj/Debug/net8.0/DavinTI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DavinTI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ff03654baf7f24e42e9556324d0bc6c158be577")]
 [assembly: System.Reflection.AssemblyProductAttribute("DavinTI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DavinTI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
